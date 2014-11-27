59ea00de3269a53e370fa9a5c4ae14cb9151a4e5
ChienliMa <maqianlie@gmail.com>
2014-11-27 14:17:38 +0800

    init

---
+++ spec/schools_spec.rb
@@ -0,0 +1,14 @@
+describe "School" do
+  
+  subject{ ::Schools.School.new('elementary')}
+
+  it "have maximum page number" do
+  end
+
+  it "can parse one page url" do
+  end
+
+  it "can get one school info" do
+  end 
+
+end
